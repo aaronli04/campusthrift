1167471d530e38e5cfd94a06b91244acb4400bdc
Aaron <aaronli5260@gmail.com>
2023-02-22 22:26:29 -0600

    start of api in next

--- pages/api/buy/showListing.ts
+++ pages/api/buy/showListing.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import currentListings from '../../../components/utility/data/listingsData/currentListings'
+import { Item } from '../../../hooks/types'
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Item[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(currentListings)
 }
